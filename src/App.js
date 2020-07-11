a13eaae100e341b0aa56fe8bbdd20c847b7fa729
abdomashael <abdomashael@yahoo.com>
2020-07-11 17:01:11 +0200

    sections preview

--- src/App.js
+++ src/App.js
@@ -3,121 +3,29 @@ import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import CarosalMain from "./components/carosal_main/carosal_main";
 import SectionItem from "./components/section_item/section_item";
+import Section from "./components/section/section";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div>
-      <CarosalMain
-        vipHidden={true}
-        sessionHidden={false}
-        sessionNo="1"
-        mainInfo="فنان ، فنان "
-        description=" تخضع كانج ميراي لعملية تجميلية للحصول على الشكل الذي طالما
+        <CarosalMain
+          vipHidden={true}
+          sessionHidden={false}
+          sessionNo="1"
+          mainInfo="فنان ، فنان "
+          description=" تخضع كانج ميراي لعملية تجميلية للحصول على الشكل الذي طالما
                   حلمت به، ولكن عندما تقع في حب دو كيانج سيوك الكاره للعمليات
                   التجميلية، تجد نفسها أمام تحد جديد."
-        mainImgSrc="https://shahidstatic3.akamaized.net/mediaObject/slider/Ihsan/Thumbs-5/Sakoon_Jamila_Slider_Hero_New/original/Sakoon_Jamila_Slider_Hero_New.jpg?height=432&width=768&croppingPoint=&version=1&type=webp"
-        thumbnailSrc="https://shahidstatic1.akamaized.net/mediaObject/New-Thumbs/Karim2020-22/Sakon-jamlia-logo/original/Sakon-jamlia-logo.png?height=&width=345&croppingPoint=mc&version=1&type=png"
-      />
-      </div>
-     
+          mainImgSrc="https://shahidstatic3.akamaized.net/mediaObject/slider/Ihsan/Thumbs-5/Sakoon_Jamila_Slider_Hero_New/original/Sakoon_Jamila_Slider_Hero_New.jpg?height=432&width=768&croppingPoint=&version=1&type=webp"
+          thumbnailSrc="https://shahidstatic1.akamaized.net/mediaObject/New-Thumbs/Karim2020-22/Sakon-jamlia-logo/original/Sakon-jamlia-logo.png?height=&width=345&croppingPoint=mc&version=1&type=png"
+        />
 
-      <div className="main">
-
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        <div style={{marginRight:"10px" , marginTop:"20px"}}>
+        <Section/>
+        <Section/>
+        <Section/>
+        <Section/>
         </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-        <div>
-          <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-        </div>
-      </div>
     </div>
   );
 }
