239d710c358e978c18a3a3c86e6c95b16e0377dd
abdomashael <abdomashael@yahoo.com>
2020-09-06 23:44:47 +0200

    create components

---
+++ src/components/pagenation/pagenation.js
@@ -0,0 +1,68 @@
+import React, { Fragment, useState, useEffect } from "react";
+import styles from "./pagenation.module.css";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+const PageNo = (props) => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    setActive(props.pageNo === props.active);
+    console.log("page", props.pageNo);
+  }, [props.pageNo]);
+
+  return (
+    <Link
+      to={`/movie/${props.pageNo}`}
+      className={active ? styles.active : styles.inactive}
+    >
+      {props.pageNo}
+    </Link>
+  );
+};
+
+const Pagnetion = (props) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loaderHidden, setLoaderHidden] = useState(true);
+
+  const showMoreOnClickHandler = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    console.log("currentPage", currentPage);
+    props.setPageNo(currentPage);
+  }, [currentPage]);
+
+  // const [pages, setPages] = useState([]);
+
+  //   useEffect(() => {
+  //     const newPages = [];
+  //     for (let index = 0; index < props.pages; index++) {
+  //       const page = <PageNo pageNo={index + 1} active={1} />;
+  //       newPages.push(page);
+  //     }
+  //     setPages([...pages, newPages]);
+  //   }, [props.pages]);
+
+  return (
+    <div>
+      <div hidden={!props.loading} className={styles.loader_conatiner}>
+        <span className={styles.loader1}>
+          <span className={styles.loader2}></span>
+        </span>
+      </div>
+      <span hidden={props.loading} className={styles.pagenation} onClick={showMoreOnClickHandler}>
+        Show More
+      </span>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    loading: state.general.isLoadingData,
+  };
+};
+
+export default connect(mapStateToProps)(Pagnetion);
