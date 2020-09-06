7b20965f63b6742831c850918d397d5052386664
abdomashael <abdomashael@yahoo.com>
2020-09-06 23:43:49 +0200

    navbar lang change

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,11 +9,14 @@ import {
   NavLink,
 } from "react-bootstrap";
 
+import { Link } from "react-router-dom";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../static/logo.svg";
 import cssClasses from "./navbar.module.css";
 import "./navbar.css";
+import Logo from "../../components/logo/logo";
 
 const CustomNavbar = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -37,36 +40,33 @@ const CustomNavbar = () => {
   return (
     <Navbar className={classes} expand="lg" variant="dark">
       <Navbar.Brand href="#home">
-        <img id="logo" src={logo} alt="logo" />
+        <Logo/>
+        {/* <img id="logo" src={"https://f1.pngfuel.com/png/119/663/684/facebook-art-masters-degree-school-logo-licentiate-baccalaureus-bird-owl-png-clip-art.png"} alt="logo" /> */}
       </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className={"ml-auto mr-3 "}>
-          <Nav.Link href="#home">
-            الرئيسية
-          </Nav.Link>
-          <Nav.Link href="#link">مسلسلات وبرامج</Nav.Link>
-          <Nav.Link href="#link1">أفلام</Nav.Link>
-          <Nav.Link href="#link2">تصفح</Nav.Link>
-          <Nav.Link href="#link3">مباشر</Nav.Link>
+        <Nav className={"mr-auto"}>
+          <Link className="nav-link active" to="/">Home</Link>
+          <Link className="nav-link" to="/movies">Movies</Link>
+          <Link className="nav-link" to="/series">Series</Link>
+          <Link className="nav-link" href="#link2">Kids</Link>
+          {/* <Nav.Link href="#link3">مباشر</Nav.Link> */}
         </Nav>
 
         <Nav.Link href="#link2">
           <FontAwesomeIcon icon={faSearch} inverse />
         </Nav.Link>
-
-        <Nav.Link  href="#link2">
-          الدخول
-        </Nav.Link>
-        <Nav.Link id="kids" href="#link2">
-          جونيورز
-        </Nav.Link>
-
+        <Nav.Link href="#link2">Login</Nav.Link>
+        {/* 
         <Nav.Link id="btn" className={cssClasses.button + " pl-4 pr-4 ml-4"}>
-          <span className="pr-2">جرب شاهد</span>
-          <span className="pl-2">VIP</span>
+          <span className="pr-2">Sign in</span>
+        </Nav.Link> */}
+
+        <Nav.Link id="btn" className={cssClasses.button + " pl-3 pr-3 ml-4"}>
+          <span className="pr-2">Sign up</span>
         </Nav.Link>
       </Navbar.Collapse>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
     </Navbar>
   );
 };
