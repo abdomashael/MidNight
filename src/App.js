e85e402fdfe5a9507c297873b144bee34ad29aec
abdomashael <abdomashael@yahoo.com>
2020-09-06 23:42:05 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
-import Home from "./pages/home"
+import Home from "./pages/home";
+import About from "./pages/about";
+import Movies from "./pages/movies/movies";
+import Series from "./pages/series";
+
+import Soon from "./components/comming_soon/soon";
+
 import { connect } from "react-redux";
 import { ADD_THUMBNAILS, ADD_TRENDS, ADD_GENRES } from "./redux/actions";
 
 import axios from "axios";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Footer from "./components/footer/footer";
 
 function App(props) {
-
   useEffect(() => {
-    
-    let fetchData = async()=>{
-      console.log("data");
-      let response = await axios.get(
-        process.env.REACT_APP_API_URL + "/trending/all/week"
-      );
-      let trends = response.data.results;
-      props.setTrends(trends)
-  
-      let thumbnails = trends.reduce(
-        (total, current) => [
-          ...total,
-          process.env.REACT_APP_IMAGE_BASE_URL + current.poster_path,
-        ],
-        []
-      );
-      props.setThumbnails(thumbnails);
-      console.log(thumbnails); 
-    }
-    let fetchGenres = async()=>{
+    let fetchGenres = async () => {
       console.log("genre");
 
       let response = await axios.get(
@@ -38,56 +26,43 @@ function App(props) {
       );
       // console.log(response.data.genres);
       // let trends = response.data.results;
-      props.setGenres(response.data.genres)
+      props.setGenres(response.data.genres);
 
-      fetchData()
-    }
-  
-    console.log("ssssss");
+    };
+
     fetchGenres();
-    }, []);
-
-  
+  }, []);
 
   return (
     <div className="App">
-      <Navbar />
-      <Home/>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/movies">
+            {/* <Soon/> */}
+            <Movies />
+          </Route>
+          <Route path="/series">
+            <Soon />
 
-      {/* <div className="blockDiv">
-        <CarosalMain
-          vipHidden={true}
-          sessionHidden={false}
-          sessionNo="1"
-          mainInfo="فنان  ، فنان "
-          description=" تخضع كانج ميراي لعملية تجميلية للحصول على الشكل الذي طالما
-                  حلمت به، ولكن عندما تقع في حب دو كيانج سيوك الكاره للعمليات
-                  التجميلية، تجد نفسها أمام تحد جديد."
-          mainImgSrc="https://shahidstatic3.akamaized.net/mediaObject/slider/Ihsan/Thumbs-5/Sakoon_Jamila_Slider_Hero_New/original/Sakoon_Jamila_Slider_Hero_New.jpg?height=432&width=768&croppingPoint=&version=1&type=webp"
-          thumbnailSrc="https://shahidstatic1.akamaized.net/mediaObject/New-Thumbs/Karim2020-22/Sakon-jamlia-logo/original/Sakon-jamlia-logo.png?height=&width=345&croppingPoint=mc&version=1&type=png"
-        />
-      </div>
-      <div className="section">
-        <Section />
-        <Section />
-        <Section />
-        <Section />
-        <Footer/>
-
-      </div> */}
-
+            {/* <Series /> */}
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
 
-
 const mapDispatchToProps = (dispatch) => {
   return {
-    setThumbnails: (thumbnails) =>
-      dispatch({ type: ADD_THUMBNAILS, payload: { thumbnails: thumbnails } }),
-      setTrends: (trends) =>
-      dispatch({ type: ADD_TRENDS, payload: { trends: trends } }),
-      setGenres: (genres) =>
+    setGenres: (genres) =>
       dispatch({ type: ADD_GENRES, payload: { genres: genres } }),
   };
 };
