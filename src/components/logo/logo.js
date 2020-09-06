fadfdaa53228db00152f67186cec3955aa56185e
abdomashael <abdomashael@yahoo.com>
2020-09-06 23:43:16 +0200

    create logo

---
+++ src/components/logo/logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styles from "./logo.module.css";
+
+const Logo = (props)=>{
+    return(
+        <div className={props.logo? props.logo:styles.logo} >
+            <span className={styles.mid}>Mid</span>
+            <span className={styles.night}>Night</span>
+        </div>
+    )
+}
+
+export default Logo;
