cfaa1346d11ea0a370e3236934bba68de84f560c
abdomashael <abdomashael@yahoo.com>
2020-10-13 16:01:59 +0200

    fix bug

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import {
-  Navbar,
-  Nav,
- 
+    Navbar,
+    Nav,
+
 } from "react-bootstrap";
 
-import { Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
 import logo from "../../static/logo.svg";
 import cssClasses from "./navbar.module.css";
 import "./navbar.css";
@@ -16,74 +16,76 @@ import Logo from "../../components/logo/logo";
 import cx from "classnames";
 
 const CustomNavbar = () => {
-  const [scrolled, setScrolled] = useState(false);
-  const [activeClasses, setActive] = useState("nav-link active");
+    const [scrolled, setScrolled] = useState(false);
+    const [activeClasses, setActive] = useState("nav-link active");
 
-  let location = useLocation();
+    let location = useLocation();
 
-  useEffect(() => {
-    window.onscroll = function () {
-      if (window.pageYOffset >= 100) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-  }, []);
-
-  return (
-    <Navbar className={cx("fixed-top",scrolled ? cssClasses.dark : cssClasses.transparent)} expand="lg" variant="dark">
-      <Navbar.Brand>
-        <Link
-          className={location.pathname === "/" ? activeClasses : "nav-link"}
-          to="/"
-        >
-          <Logo />
-        </Link>      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className={"mr-auto"}>
-          <Link
-            className={location.pathname === "/" ? activeClasses : "nav-link"}
-            to="/"
-          >
-            Home
-          </Link>
-          <Link
-            className={
-              location.pathname === "/movies" ? activeClasses : "nav-link"
+    useEffect(() => {
+        window.onscroll = function () {
+            if (window.pageYOffset >= 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
             }
-            to="/movies"
-          >
-            Movies
-          </Link>
-          <Link className="nav-link" to="/series">
-            Series
-          </Link>
-          <Link className="nav-link" to="/series">
-            Kids
-          </Link>
-          {/* <Nav.Link href="#link3">مباشر</Nav.Link> */}
-        </Nav>
+        };
+    }, []);
 
-        <Nav.Link className="pb-0" href="#link2">
-          <FontAwesomeIcon icon={faSearch} inverse />
-        </Nav.Link>
-        <Link className="nav-link" to="/auth?type=login">
-          Login
-        </Link>
+    return (
+        <Navbar className={cx("fixed-top", scrolled ? cssClasses.dark : cssClasses.transparent)} expand="lg"
+                variant="dark">
+            <Navbar.Brand>
+                <Link
+                    className={location.pathname === "/" ? activeClasses : "nav-link"}
+                    to="/"
+                >
+                    <Logo/>
+                </Link> </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className={"mr-auto"}>
+                    <Link
+                        className={location.pathname === "/" ? activeClasses : "nav-link"}
+                        to="/"
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        className={
+                            location.pathname === "/movies" ? activeClasses : "nav-link"
+                        }
+                        to="/movies"
+                    >
+                        Movies
+                    </Link>
+                    <Link className={location.pathname === "/series" ? activeClasses : "nav-link"
+                    } to="/series">
+                        Series
+                    </Link>
+                    {/*<Link className="nav-link" to="/series">*/}
+                    {/*    Kids*/}
+                    {/*</Link>*/}
+                    {/* <Nav.Link href="#link3">مباشر</Nav.Link> */}
+                </Nav>
 
-        <Link
-          to="/auth/type=signup"
-          id="btn"
-          className={cx(cssClasses.button, "nav-link")}
-        >
-          Sign up
-        </Link>
-      </Navbar.Collapse>
-      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-    </Navbar>
-  );
+                <Nav.Link className="pb-0" href="#link2">
+                    <FontAwesomeIcon icon={faSearch} inverse/>
+                </Nav.Link>
+                <Link className="nav-link" to="/auth?type=login">
+                    Login
+                </Link>
+
+                <Link
+                    to="/auth/type=signup"
+                    id="btn"
+                    className={cx(cssClasses.button, "nav-link")}
+                >
+                    Sign up
+                </Link>
+            </Navbar.Collapse>
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        </Navbar>
+    );
 };
 
 export default CustomNavbar;
